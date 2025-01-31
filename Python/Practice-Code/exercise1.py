f625ecc25b2a59dc6d8877b6d4d85b0fc2c781d6
DJ <dj.leamen@ontariotechu.net>
2025-01-30 21:02:28 -0500

    Python L1

---
+++ Python/Practice-Code/exercise1.py
@@ -0,0 +1,14 @@
+# 1.	Create a variable named my_name and store your name as a string.
+
+# 2.	Create a variable named my_age and store your age as an integer.
+
+# 3.	Create a variable named my_height and store your height as a float.
+
+print(my_name, my_age, my_height)
+
+# 4.	Calculate how old you will be in 50 years.
+current_age = 
+years_later = 
+future_age = 
+
+print("In 50 years, I will be", future_age, "years old.")
