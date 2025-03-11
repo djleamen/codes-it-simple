4bee5f92e3b5390f146fed0da8dbfc1c5b1f3e22
DJ <dj.leamen@ontariotechu.net>
2025-03-11 16:42:12 -0400

    Added lessons 3, 4 for Python

---
+++ Python/Practice-Code/exercise2.py
@@ -0,0 +1,16 @@
+# 1.    Ask the user for their name
+
+# 2.    Ask the user for their age
+
+# 3.    Ask the user for their height
+
+# 4.    Print out the user's name, age, and height, ensuring that the output is formatted as follows:
+#       Name: [name]
+#       Age: [age]
+#       Height: [height]
+#       (where [name], [age], and [height] are the values the user entered)
+
+# 5.    Calculate how old the user will be in 50 years and print out the result in the following format:
+#       In 50 years, [name] will be [future_age] years old.
+#       (where [name] is the user's name and [future_age] is the calculated age)    
+#       (Hint: You will need to convert the user's age to an integer before performing the calculation)
---
+++ Python/Practice-Code/exercise3.py
@@ -0,0 +1,18 @@
+# 1.    Ask the user for their age. If the user's age is less than 18, print out "You are a minor." If the user's age is 18 or older, print out "You are an adult."
+
+input_age = input("Enter your age: ")
+if 
+else
+
+# 2.    Iterate over a list of numbers from 1 to 10 (inclusive).
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for
+
+# 3.    For each number in a list of 1-10, print out whether the number is even or odd.
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for
+    ___ number % 2 == 0:
+       
+    ___:
